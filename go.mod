d96b4f05b4581529b1a348919849508ea928b844
RPC183 <rpc183@RPC183-MacBook-Pro.local>
2020-10-26 09:09:58 +0900

    [add]mysql connection

---
+++ go.mod
@@ -0,0 +1,9 @@
+module work
+
+go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/kelseyhightower/envconfig v1.4.0
+)
--- main.go
+++ main.go
@@ -8,70 +8,120 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/kelseyhightower/envconfig"
 )
 
-type Page struct {
+// Article is struct
+type Article struct {
+	ID    int
 	Title string
 	Body  []byte
 }
 
+// Env is struct
 type Env struct {
-	DB_PORT        int
-	MYSQL_DATABASE string
+	DB_CONNECTION string
+	DB_PORT       string
+	DB_HOST       string
+	DB_USERNAME   string
+	DB_PASSWORD   string
+	DB_DATABASE   string
 }
 
-func (p *Page) save() error {
+// func gormConnect() *gorm.DB {
+// 	var goenv Env
+// 	err := envconfig.Process("", &goenv)
+// 	if err != nil {
+// 		log.Fatal(err.Error())
+// 	}
+// 	PROTOCOL := "tcp(" + goenv.DB_HOST + ":" + goenv.DB_PORT + ")"
+// 	CONNECT := goenv.DB_USERNAME + ":" + goenv.DB_PASSWORD + "@" + PROTOCOL + "/" + goenv.DB_DATABASE
+// 	db, err := gorm.Open(goenv.DB_CONNECTION, CONNECT)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return db
+// }
+
+func DBConnect() (db *sql.DB) {
+	var goenv Env
+	err2 := envconfig.Process("", &goenv)
+	if err2 != nil {
+		panic(err2.Error())
+	}
+	PROTOCOL := "tcp(" + goenv.DB_HOST + ":" + goenv.DB_PORT + ")"
+
+	CONNECT := goenv.DB_USERNAME + ":" + goenv.DB_PASSWORD + "@" + PROTOCOL + "/" + goenv.DB_DATABASE
+	db, err := sql.Open("mysql", CONNECT)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func (p *Article) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func (p *Page) delete() error {
+func (p *Article) delete() error {
 	filename := p.Title + ".txt"
 	fmt.Println(filename)
 	return os.Remove(filename)
 }
 
-func loadPage(title string) (*Page, error) {
+func loadArticle(title string) (*Article, error) {
+	// db := DBConnect()
+	// defer db.Close()
+	// var body string
+	// // stmtOut, err := db.Prepare(fmt.Sprintf("SELECT * FROM articles where title = ?", title)) //
+	// err := db.QueryRow("SELECT * FROM articles where title = ?", title).Scan(&body)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// fmt.Println(body)
+	// fmt.Println(title)
+
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}, nil
+	return &Article{Title: title, Body: body}, nil
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+func renderTemplate(writer http.ResponseWriter, tmpl string, article *Article) {
 	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
+	err := t.Execute(writer, article)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, err := loadPage(title)
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	article, err := loadArticle(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
-	renderTemplate(w, "view", p)
+	renderTemplate(w, "view", article)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-	p, err := loadPage(title)
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	article, err := loadArticle(title)
 	if err != nil {
-		p = &Page{Title: title}
+		article = &Article{Title: title}
 	}
-	renderTemplate(w, "edit", p)
+	renderTemplate(w, "edit", article)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	article := &Article{Title: title, Body: []byte(body)}
+	err := article.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -79,48 +129,76 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
-func deleteHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(len("/delete/"))
-	title := r.URL.Path[len("/delete/"):]
+func deleteHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.delete()
+	article := &Article{Title: title, Body: []byte(body)}
+	err := article.delete()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view|delete)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		fmt.Println(m)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
 }
 
 func main() {
-	var goenv Env
-	envconfig.Process("", &goenv)
-	fmt.Println(goenv.DB_PORT)
-	fmt.Println(goenv.MYSQL_DATABASE)
 
-	db, err := sql.Open("mysql", "admin:secret@tcp(db-host:3306)/test")
-	if err != nil {
-		panic(err.Error())
-	}
+	db := DBConnect()
+	// db := gormConnect()
 	defer db.Close()
+	var article Article
+	// var id string
+	// var title string
+	// var body string
 
-	// rows, err := db.Query("show tables") //
-	rows, err := db.Query("SELECT * FROM articles") //
+	article.ID = 1
+	// db.First(&article)
+	// stmtOut, err := db.Prepare(fmt.Sprintf("SELECT * FROM articles where title = ?", title)) //
+	err := db.QueryRow("SELECT * FROM articles where title = ?", "title1").Scan(&article.ID, &article.Title, &article.Body)
+	// err := db.QueryRow("SELECT * FROM articles where title = ?", "title1")
+	// fmt.Println(err)
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println(rows)
+	fmt.Println(article.ID)
+	fmt.Println(article.Title)
+	fmt.Println(article.Body)
+	fmt.Println("---------")
+	// fmt.Println(title)
 
-	columns, err := rows.Columns() // カラム名を取得
-	if err != nil {
-		panic(err.Error())
-	}
+	// db := DBConnect()
+	// defer db.Close()
 
-	fmt.Println(columns)
+	// // rows, err := db.Query("show tables") //
+	// rows, err := db.Query("SELECT * FROM articles") //
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// fmt.Println(rows)
 
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
-	http.HandleFunc("/delete/", deleteHandler)
+	// columns, err := rows.Columns() // カラム名を取得
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// fmt.Println(columns)
+
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/delete/", makeHandler(deleteHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
