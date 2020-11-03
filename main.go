7b7de80024b9917df04df556903a9071358c058d
RPC183 <rpc183@RPC183-MacBook-Pro.local>
2020-11-03 21:01:11 +0900

    [add] keyをTitleからIDに変更

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
-	"regexp"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -14,9 +14,11 @@ import (
 
 // Article is struct
 type Article struct {
-	ID    int
-	Title string
-	Body  []byte
+	ID        int
+	Title     string
+	Body      []byte
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 // Env is struct
@@ -37,7 +39,8 @@ func gormConnect() *gorm.DB {
 	}
 	PROTOCOL := "tcp(" + goenv.DB_HOST + ":" + goenv.DB_PORT + ")"
 	CONNECT := goenv.DB_USERNAME + ":" + goenv.DB_PASSWORD + "@" + PROTOCOL + "/" + goenv.DB_DATABASE
-	db, err := gorm.Open(goenv.DB_CONNECTION, CONNECT)
+	OPTION := "?parseTime=true&loc=Asia%2FTokyo"
+	db, err := gorm.Open(goenv.DB_CONNECTION, CONNECT+OPTION)
 	if err != nil {
 		panic(err)
 	}
@@ -47,9 +50,11 @@ func gormConnect() *gorm.DB {
 func (article *Article) save() {
 	db := gormConnect()
 	defer db.Close()
+	title := article.Title
 	body := article.Body
 
-	db.First(&article, "title=?", article.Title)
+	db.First(&article, "id=?", article.ID)
+	article.Title = title
 	article.Body = body
 
 	db.Save(&article)
@@ -59,20 +64,20 @@ func (article *Article) delete() {
 	db := gormConnect()
 	defer db.Close()
 
-	db.First(&article, "title=?", article.Title)
+	db.First(&article, "id=?", article.ID)
 	db.Delete(&article)
 }
 
-func loadArticle(title string) (*Article, error) {
+func loadArticle(id int) (*Article, error) {
 	var article Article
 
 	db := gormConnect()
 	defer db.Close()
 
-	db.First(&article, "title=?", title)
+	db.First(&article, "id=?", id)
 
+	title := article.Title
 	body := article.Body
-	id := article.ID
 
 	return &Article{ID: id, Title: title, Body: body}, nil
 }
@@ -88,64 +93,6 @@ func loadAllArticle() []Article {
 	return articles
 }
 
-func renderTemplate(writer http.ResponseWriter, tmpl string, article interface{}) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	err := t.Execute(writer, article)
-	if err != nil {
-		panic(err.Error())
-	}
-}
-
-func listHandler(writer http.ResponseWriter, r *http.Request) {
-	articles := loadAllArticle()
-	renderTemplate(writer, "list", articles)
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	article, err := loadArticle(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", article)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	article, err := loadArticle(title)
-	if err != nil {
-		article = &Article{Title: title}
-	}
-	renderTemplate(w, "edit", article)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	article := &Article{Title: title, Body: []byte(body)}
-	article.save()
-
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-func deleteHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	article := &Article{Title: title, Body: []byte(body)}
-	article.delete()
-	http.Redirect(w, r, "/list/", http.StatusFound)
-}
-
-var validPath = regexp.MustCompile("^/(list|edit|save|view|delete)/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
-}
-
 func ListArticles(c *gin.Context) {
 	articles := loadAllArticle()
 	c.HTML(200, "list.html", gin.H{
@@ -154,8 +101,8 @@ func ListArticles(c *gin.Context) {
 }
 
 func EditArticles(c *gin.Context) {
-	title := c.Param("title")
-	article, err := loadArticle(title)
+	id, _ := strconv.Atoi(c.Param("id"))
+	article, err := loadArticle(id)
 	if err != nil {
 		// リダイレクトする
 		c.Redirect(http.StatusNotFound, "404.html")
@@ -167,25 +114,26 @@ func EditArticles(c *gin.Context) {
 }
 
 func SaveArticles(c *gin.Context) {
+	id, _ := strconv.Atoi(c.PostForm("id"))
 	title := c.PostForm("title")
 	body := c.PostForm("body")
-	article := &Article{Title: title, Body: []byte(body)}
+	article := &Article{ID: id, Title: title, Body: []byte(body)}
 	article.save()
 	c.Redirect(http.StatusSeeOther, "/list")
 }
 
 func DeleteArticle(c *gin.Context) {
-	title := c.Param("title")
+	id, _ := strconv.Atoi(c.Param("id"))
 	db := gormConnect()
 	defer db.Close()
-	article := &Article{Title: title}
+	article := &Article{ID: id}
 	article.delete()
 	c.Redirect(http.StatusSeeOther, "/list")
 }
 
 func ViewArticles(c *gin.Context) {
-	title := c.Param("title")
-	article, err := loadArticle(title)
+	id, _ := strconv.Atoi(c.Param("id"))
+	article, err := loadArticle(id)
 	if err != nil {
 		// リダイレクトする
 		c.Redirect(http.StatusNotFound, "404.html")
@@ -196,27 +144,23 @@ func ViewArticles(c *gin.Context) {
 	})
 }
 
+func TopArticles(c *gin.Context) {
+	c.Redirect(http.StatusSeeOther, "/list")
+}
+
+func NewArticles(c *gin.Context) {
+	c.HTML(200, "edit.html", gin.H{})
+}
+
 func main() {
 	router := gin.Default()
-
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
-	})
 	router.LoadHTMLGlob("templates/*")
+	router.GET("/", TopArticles)
+	router.GET("/new", NewArticles)
 	router.GET("/list", ListArticles)
-	router.GET("/view/:title", ViewArticles)
-	router.GET("/edit/:title", EditArticles)
-	router.GET("/delete/:title", DeleteArticle)
+	router.GET("/view/:id", ViewArticles)
+	router.GET("/edit/:id", EditArticles)
+	router.GET("/delete/:id", DeleteArticle)
 	router.POST("/save", SaveArticles)
-
 	router.Run(":8080")
-
-	// http.HandleFunc("/list/", listHandler)
-	// http.HandleFunc("/view/", makeHandler(viewHandler))
-	// http.HandleFunc("/edit/", makeHandler(editHandler))
-	// http.HandleFunc("/save/", makeHandler(saveHandler))
-	// http.HandleFunc("/delete/", makeHandler(deleteHandler))
-	// log.Fatal(http.ListenAndServe(":8080", nil))
 }
