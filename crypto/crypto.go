bef8fed634acdecf7c95613e4264279b6b40ae39
RPC183 <rpc183@RPC183-MacBook-Pro.local>
2020-11-12 09:31:42 +0900

    [add] crypto

---
+++ crypto/crypto.go
@@ -0,0 +1,10 @@
+package crypto
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Encrypt(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash), err
+}
